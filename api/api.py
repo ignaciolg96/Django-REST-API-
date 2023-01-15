e0793905f3b87fe3eecf4943c8ba0eec337d8f67
ignaciolg96 <ignaciolopezg96@gmail.com>
2023-01-15 12:29:28 -0300

    Stats Update - WORKING

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 from .models import Medidor, Medicion
 from rest_framework import viewsets, permissions
 from .serializers import MedidorSerializer, MedicionSerializer
+from rest_framework.decorators import action
 
 
 class MedidorViewSet(viewsets.ModelViewSet):
@@ -8,7 +9,11 @@ class MedidorViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = MedidorSerializer
 
+        
 class MedicionViewSet(viewsets.ModelViewSet):
     queryset = Medicion.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = MedicionSerializer
+
+
+    
--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from decimal import Decimal
+from django.db.models import Avg, Max, Min, Sum
+
 
 # Create your models here.
 
@@ -11,10 +13,25 @@ class Medidor(models.Model):
     def __str__(self):
         return f'{self.nombre} (id: {self.llave_id})'
 
+    @property
+    def estadisticas(self):
+        consumoMaximo = Medicion.objects.all().filter(medidor=self.id).aggregate(Max('consumo_registrado_kwH'))
+        consumoMinimo = Medicion.objects.all().filter(medidor=self.id).aggregate(Min('consumo_registrado_kwH'))
+        consumoTotal = Medicion.objects.all().filter(medidor=self.id).aggregate(Sum('consumo_registrado_kwH'))
+        consumoPromedio = Medicion.objects.all().filter(medidor=self.id).aggregate(Avg('consumo_registrado_kwH'))
+        estadisticas = {
+            'Consumo Maximo':consumoMaximo.get('consumo_registrado_kwH__max'),
+            'Consumo Minimo':consumoMinimo.get('consumo_registrado_kwH__min'),
+            'Consumo Total':consumoTotal.get('consumo_registrado_kwH__sum'),
+            'Consumo Promedio':consumoPromedio.get('consumo_registrado_kwH__avg')
+        }
+        return estadisticas
+
 class Medicion(models.Model):
-    medidor = models.ForeignKey('Medidor', 
-        related_name='Medicion', 
-        on_delete=models.CASCADE,
+    medidor = models.ForeignKey(
+        'Medidor', 
+        related_name='mediciones', 
+        on_delete=models.DO_NOTHING,
         )
 
     fecha = models.DateTimeField(auto_now_add=True) 
@@ -24,3 +41,7 @@ class Medicion(models.Model):
         decimal_places=3,
         validators=[MinValueValidator(Decimal('0'))]
         )
+
+    def __str__(self):
+        return f'Consumo: {self.consumo_registrado_kwH} kwH)'
+
